084bd59e7d972c48cb4cb95802f916997759e658
jdalton92 <jamesdalton7648@gmail.com>
2020-11-09 19:49:04 +1100

    update asset tasks

--- api/api/assets/tasks.py
+++ api/api/assets/tasks.py
@@ -1,9 +1,12 @@
+import json
 import logging
 
 from api.config import celery_app
 from api.core.utils import TradeApiRest
+from django.forms.models import model_to_dict
 
 from .models import Asset, AssetClass, Exchange
+from .serializers import AssetSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -20,39 +23,34 @@ def update_asset_models():
     assets = api._list_assets()
 
     # Get list of unique exchanges
-    exchanges = set(map(lambda asset: asset['exchange'], assets))
-    asset_classes = set(map(lambda asset: asset['asset_class'], assets))
+    exchanges = set(map(
+        lambda asset: asset.__dict__['_raw']['exchange'],
+        assets
+    ))
+    asset_classes = set(map(
+        lambda asset: asset.__dict__['_raw']['class'],
+        assets
+    ))
 
     # Set up total count of additions to database
     additions_to_db = 0
 
     # Update exchanges
     for item in exchanges:
-        exchange, created = Exchange.objects.update_or_create(
-            name=item,
-            defaults={'is_current': 'True'}
-        )
+        _, created = Exchange.objects.get_or_create(name=item)
         if created:
             additions_to_db += 1
 
     # Update asset classes
     for item in asset_classes:
-        asset_class, created = AssetClass.objects.update_or_create(
-            name=item,
-            defaults={'is_current': 'True'}
-        )
+        _, created = AssetClass.objects.get_or_create(name=item)
         if created:
             additions_to_db += 1
 
     # From list of assets, create or update model instances
-    for a in assets:
-        asset, created = Asset.objects.update_or_create(
-            **a,
-            defaults={'is_current': 'True'}
-        )
+    for item in assets:
+        _, created = Asset.objects.get_or_create(**item)
         if created:
             additions_to_db += 1
 
     logger.info(f"Updates: {additions_to_db}")
-
-    # CHECK IF EXISTS IN DB BUT NOT IN NEW LIST, AND SET IS_CURRENT TO FALSE
